e8bf0737278f79840ec2b9f3d99286420ae4878c
Danil Vdovin <felixffox@yandex.ru>
2023-05-03 15:15:49 +0300

    Проверка отображения тегов и ингредиентов в созданном рецепте

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -155,6 +155,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 6,
+    'PAGINATE_BY_PARAM': 'limit',
     'UPLOADED_FILES_USE_URL': False,
 }
 
