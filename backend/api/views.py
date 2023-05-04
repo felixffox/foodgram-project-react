43bb17b3b40b2790e71cbe0829ffa228351b9948
Danil Vdovin <felixffox@yandex.ru>
2023-05-04 19:51:59 +0300

    Проверка отображения тегов и ингредиентов в созданном рецепте

--- backend/api/views.py
+++ backend/api/views.py
@@ -108,9 +108,6 @@ class RecipeViewSet(viewsets.ModelViewSet, ActionMethods):
     filter_backends = (DjangoFilterBackend, )
     filterset_class = RecipeFilter
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
     def get_serializer_class(self):
         if self.request.method in ('POST', 'PATCH'):
             return CreateRecipeSerializer
